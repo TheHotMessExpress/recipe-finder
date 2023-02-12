dbfaac17b204dba12c72609f73425b771af60112
Andrew Moots <amoots@emich.edu>
2023-02-12 13:31:49 -0500

    feat: Add real call and response with Spoontacular API
    * Calls complexSearch Spoontacular API
    * Formats and returns recipe list matching query string

--- backend/search_recipes.php
+++ backend/search_recipes.php
@@ -1,6 +1,7 @@
 <?php
 
 require("config.php");
+require("./functions/func_recipes.php");
 
 header("Content-type:application/json");
 
@@ -8,8 +9,11 @@ header("Content-type:application/json");
 $query_string = (isset($_GET['query']) ? $_GET['query'] : "");
 
 if($query_string != ""){
+    // fetch info from spoontacular
+    $recipes = getRecipes($query_string);
+
     // stub out response for front end
-    echo json_encode(array("success" => true, "data" => array(array("title" => "Recipe #1"), array("title" => "Recipe #2"))));
+    echo json_encode(array("success" => true, "data" => array($recipes)));
 }else{
     // invalid (empty) string
     echo json_encode(array("success" => false, "error" => array("message" => "Invalid (empty) query string")));

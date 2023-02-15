5deca2e01d2ad02be343f03129dd756b86d00f6a
Camden Fisher <cfishe24@emich.edu>
2023-02-15 16:24:54 -0500

    feat: Stub out API endpoint for view recipe page
    * Change made so frontend developers can continue developement with API endpoint while
    contiuing to develop backend functionality.

---
+++ backend/recipe_information.php
@@ -0,0 +1,9 @@
+<?php
+
+require("config.php");
+
+header("Content-type:application/json");
+
+    // stub out response for front end
+    echo json_encode(array("success" => true, "title" => "TestTitle", "image" => "https://spoonacular.com/recipeImages/716429-556x370.jpg", "ingredients" => array("Ingredient1", "Ingredient2"), "source" => "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html"));
+?>

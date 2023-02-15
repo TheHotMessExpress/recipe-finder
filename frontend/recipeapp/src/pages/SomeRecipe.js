75a5442a58affb179c54d3d4886312965a16a35f
mhenning3 <35973769+mhenning3@users.noreply.github.com>
2023-02-15 16:10:52 -0500

    feature: outlined display page
    Basic display on recipe page
    issue #3

--- frontend/recipeapp/src/pages/SomeRecipe.js
+++ frontend/recipeapp/src/pages/SomeRecipe.js
@@ -1,11 +1,46 @@
 import React from "react";
-  
+import '../index.css';
+
 const SomeRecipe = () => {
-  return (
-    <div>
+    return (
+        <div className="main">
+        
       <h1>
-        This is the "SomeRecipe" Page
+        This is the "Some Recipe" Page
       </h1>
+          <body>
+                <p1 id = "base">
+
+                    <img src="https://sugarspunrun.com/wp-content/uploads/2016/03/easy-chocolate-cupcakes-1-of-1-6.jpg" alt="cupcake" />
+
+                    <h3 > I am a title</h3>
+                    <h4 >
+                        <ul>
+                            <li>I am a list</li>
+                            <li>I am a list too</li>
+                            <li>I am a list as well</li>
+                        </ul>
+                    </h4>
+                    <h5>
+                        <ol>
+                            <li>These are instructions</li><br></br>
+                            <li>look at me</li><br></br>
+                            <li>yee</li><br></br>
+                            <li>I am testing</li><br></br>
+                            <li>results of</li><br></br>
+                            <li>a long list</li><br></br>
+                            <li>a</li><br></br>
+                            <li>a</li><br></br>
+                            <li>a</li><br></br>
+                            <li>a</li><br></br>
+                            
+                        </ol>
+                    </h5>
+
+
+              </p1>
+        
+          </body>
     </div>
   );
 };

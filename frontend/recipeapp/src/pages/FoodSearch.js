7df176061b1223c9f548463affe0c1db1d77b02d
Andrew Moots <amoots@emich.edu>
2023-02-16 18:23:50 -0500

    feat/fix: Integrate live API calls to backend and stub out response handling
    * refactor existing API call code
    * implement config variable for API domain
    * adjust package to keep page from erroring

--- frontend/recipeapp/src/pages/FoodSearch.js
+++ frontend/recipeapp/src/pages/FoodSearch.js
@@ -1,8 +1,8 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
-import List from "../components/List";
 import '../index.css';
+import '../config.js';
 
 const FoodSearch = () => {
   const [inputText, setInputText] = useState("");
@@ -11,38 +11,11 @@ const FoodSearch = () => {
     setInputText(lowerCase);
   };
 
-    const checkTextInput = () => {
-    alert('Success');
-  };
-
-
-  //Submit names from demo
-  const [names, setNames] = useState([]);
-  const [name, setName] = useState("");
- 
+  // call API to get recipes
   const handleSubmit = (event) => {
    console.log("handleSubmit called");
    event.preventDefault();
-   setNames([name]);
-   fetch("http://localhost:8080/add_name.php?name="+name, {
-       "method": "GET",
-       "headers": {
-         "content-type": "text/plain",
-         "accept": "application/json"
-       }
-     })
-     .then(response => response.json())
-     .then(response => {
-       console.log(response)
-     })
-     .catch(err => {
-       console.log(err);
-     });
-    setName("");
-  };
-
-    // load list of all names from demo
-    fetch("http://localhost:8080/get_names.php", {
+    fetch(global.config.api_url+"/search_recipes.php?query="+inputText, {
       "method": "GET",
       "headers": {
         "content-type": "text/plain",
@@ -51,79 +24,49 @@ const FoodSearch = () => {
     })
     .then(response => response.json())
     .then(response => {
-      setNames([...response['data']]);
+      console.log(response);
+      // display recipes
+      if(response['success'] == true){
+        // check results length
+        if(response['data'].length > 0){
+          // display each result
+          for(let i = 0; i < response['data'].length; i++){
+            // response['data'][i]['title']
+          }
+        }else{
+          // display no results msg
+        }
+      }else{
+        // display error msg
+      }
     })
     .catch(err => {
       console.log(err);
     });
- 
+  };
 
   return (
     <div className="main">
       <h1>This is "FoodSearch" Page</h1>
 
       <div>
-        <TextField className="search"
-        id="outlined-basic"
-        onChange={inputHandler}
-        variant="outlined"
-        fullWidth
-        label="Search"
-        onSubmit={handleSubmit}/>
-  
-        <button disabled={!inputText} class="FoodSearchButton" onClick={checkTextInput}>Click me</button>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <TextField className="search"
+            id="outlined-basic"
+            onChange={inputHandler}
+            variant="outlined"
+            fullWidth
+            label="Search"/>
+
+          <button disabled={!inputText} class="FoodSearchButton">Click me</button>
+        </form>
       </div>
 
-    <div className="foodSearch">
-    Recipes:
-
-    {/* display names from demo */}
-    <ul>
-        {names.map((name, index) => (
-          <p key={index}>{name}</p>
-        ))}
-    </ul>
-    
-    </div>
-
+      <div className="foodSearch">
+      Recipes:    
+      </div>
     </div>
   );
 }
 
 export default FoodSearch;
-
-
-
-// const FoodSearch = () => {
-//   const [inputText, setInputText] = useState("");
-//   let inputHandler = (e) => {
-//     var lowerCase = e.target.value.toLowerCase();
-//     setInputText(lowerCase);
-//   };
-
-//   //Checks for input in search box
-//   const checkTextInput = () => {
-//     alert('Success');
-//   };
-
-//   return (
-//     <div className="main">
-//       <h1>This is "FoodSearch" Page</h1>
-//       <div>
-//         <TextField className="search"
-//           id="outlined-basic"
-//           onChange={inputHandler}
-//           variant="outlined"
-//           fullWidth
-//           label="Search"
-//         />
-//         <button disabled={!inputText} class="FoodSearchButton" onClick={checkTextInput}>Click me</button>
-//       </div>
-//       <div className = "list">
-//       <List input={inputText} onChange={event => setInputText(event.target.value)} /> 
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FoodSearch;

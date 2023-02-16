13337ff49b52051968f85f75a0ffda0ec2a33596
Camden Fisher <cfishe24@emich.edu>
2023-02-16 18:28:01 -0500

    feat: Add input validation for id.
    * Change was made to make sure the query id given is not null or empty.
    * Makes sure recipe id being searched is a valid recipe id

--- backend/recipe_information.php
+++ backend/recipe_information.php
@@ -4,6 +4,16 @@ require("config.php");
 
 header("Content-type:application/json");
 
+// get parameters
+$query_id = (isset($_GET['query']) ? $_GET['query'] : "");
+
+if($query_id != ""){
     // stub out response for front end
-    echo json_encode(array("success" => true, "title" => "TestTitle", "image" => "https://spoonacular.com/recipeImages/716429-556x370.jpg", "ingredients" => array("Ingredient1", "Ingredient2"), "source" => "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html"));
+    echo json_encode(array("success" => true, "title" => "TestTitle", "image" => 
+    "https://spoonacular.com/recipeImages/716429-556x370.jpg", "ingredients" => array("Ingredient1", "Ingredient2"), "source" => "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html"));
+}else{
+     // invalid (empty) string
+     echo json_encode(array("success" => false, "error" => array("message" => 
+     "Invalid (empty) query id")));
+}
 ?>

03df68551715062806bf67380bfef2b766ddfcaa
jjjjess <91393792+jjjjess@users.noreply.github.com>
2023-02-17 13:04:30 -0500

    fix attempt: pull/display API when food item is searched
    - implemented setRecipes updater function for recipe array (line 9)
    - line 36 calls setRecipes to update value of recipe[ ]
    - CSS tweaked just to make searchResults div bigger - not finished with overall styling of page so adjust as needed
    
    - I'm confused by the errors that display in the console after handleSubmit is called. Pushing to branch for review

--- frontend/recipeapp/src/pages/FoodSearch.js
+++ frontend/recipeapp/src/pages/FoodSearch.js
@@ -6,44 +6,81 @@ import '../config.js';
 
 const FoodSearch = () => {
   const [inputText, setInputText] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
   let inputHandler = (e) => {
     var lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
   };
 
   // call API to get recipes
-  const handleSubmit = (event) => {
-   console.log("handleSubmit called");
-   event.preventDefault();
-    fetch(global.config.api_url+"/search_recipes.php?query="+inputText, {
-      "method": "GET",
-      "headers": {
-        "content-type": "text/plain",
-        "accept": "application/json"
-      }
-    })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
-      // display recipes
-      if(response['success'] == true){
-        // check results length
-        if(response['data'].length > 0){
-          // display each result
-          for(let i = 0; i < response['data'].length; i++){
-            // response['data'][i]['title']
-          }
-        }else{
-          // display no results msg
-        }
-      }else{
-        // display error msg
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  };
+      const handleSubmit = (event) => {
+      console.log("handleSubmit called");
+      event.preventDefault();
+       fetch(global.config.api_url+"/search_recipes.php?query="+inputText, {
+         "method": "GET",
+         "headers": {
+           "content-type": "text/plain",
+           "accept": "application/json"
+         }
+       })
+       .then(response => response.json())
+       .then(response => {
+         console.log(response);
+         // display recipes
+         if(response['success'] === true){
+           // check results length
+           if(response['data'].length > 0){
+             // display each result
+             for(let i = 0; i < response['data'].length; i++){
+               setRecipes(response['data'][i]['title']);
+             }
+           }else{
+             // display no results msg - will come back to error messages
+           }
+         }else{
+           // display error msg
+         }
+       })
+       .catch(err => {
+         console.log(err);
+       });
+     };
+
+
+    // call API to get recipes
+    // const handleSubmit = (event) => {
+    //   console.log("handleSubmit called");
+    //   event.preventDefault();
+    //    fetch(global.config.api_url+"/search_recipes.php?query="+inputText, {
+    //      "method": "GET",
+    //      "headers": {
+    //        "content-type": "text/plain",
+    //        "accept": "application/json"
+    //      }
+    //    })
+    //    .then(response => response.json())
+    //    .then(response => {
+    //      console.log(response);
+    //      // display recipes
+    //      if(response['success'] == true){
+    //        // check results length
+    //        if(response['data'].length > 0){
+    //          // display each result
+    //          for(let i = 0; i < response['data'].length; i++){
+    //            // response['data'][i]['title']
+    //          }
+    //        }else{
+    //          // display no results msg
+    //        }
+    //      }else{
+    //        // display error msg
+    //      }
+    //    })
+    //    .catch(err => {
+    //      console.log(err);
+    //    });
+    //  };
 
   return (
     <div className="main">
@@ -51,21 +88,30 @@ const FoodSearch = () => {
 
       <div>
         <form autoComplete="off" onSubmit={handleSubmit}>
-          <TextField className="search"
+
+          {/* Commented out because it makes the page blank, working with the input field below instead - not sure if this is tied to the issue? */}
+          {/* <TextField className="search"
             id="outlined-basic"
             onChange={inputHandler}
             variant="outlined"
             fullWidth
-            label="Search"/>
+            label="Search"/> */}
 
-          <button disabled={!inputText} class="FoodSearchButton">Click me</button>
+          <input className="search" type="text" onChange={inputHandler} placeholder="Search"  />
+
+          <button disabled={!inputText} className="foodSearchButton">Click me</button>
         </form>
-      </div>
 
-      <div className="foodSearch">
-      Recipes:    
+        <div id="recipe-results" className="searchResults">
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image}/>
+          </div>
+        ))}
       </div>
     </div>
+  </div>
   );
 }
 

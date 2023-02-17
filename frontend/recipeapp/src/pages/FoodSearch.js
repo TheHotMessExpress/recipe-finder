b243e6d5af965fe516269a46d8376b9754af755f
jjjjess <91393792+jjjjess@users.noreply.github.com>
2023-02-17 17:43:11 -0500

    Feat: API is searchable and results are displayed
    - handleSubmit function is completed
    - results of search are displayed in scrollable div
    - each image links to "Some-Recipe" page
    - css styling added

--- frontend/recipeapp/src/pages/FoodSearch.js
+++ frontend/recipeapp/src/pages/FoodSearch.js
@@ -1,86 +1,49 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 import { useState } from "react";
-import '../index.css';
-import '../config.js';
+import "../index.css";
+import "../config.js";
 
 const FoodSearch = () => {
   const [inputText, setInputText] = useState("");
   const [recipes, setRecipes] = useState([]);
 
   let inputHandler = (e) => {
-    var lowerCase = e.target.value.toLowerCase();
-    setInputText(lowerCase);
+    setInputText(e.target.value);
   };
 
   // call API to get recipes
-      const handleSubmit = (event) => {
-      console.log("handleSubmit called");
-      event.preventDefault();
-       fetch(global.config.api_url+"/search_recipes.php?query="+inputText, {
-         "method": "GET",
-         "headers": {
-           "content-type": "text/plain",
-           "accept": "application/json"
-         }
-       })
-       .then(response => response.json())
-       .then(response => {
-         console.log(response);
-         // display recipes
-         if(response['success'] === true){
-           // check results length
-           if(response['data'].length > 0){
-             // display each result
-             for(let i = 0; i < response['data'].length; i++){
-               setRecipes(response['data'][i]['title']);
-             }
-           }else{
-             // display no results msg - will come back to error messages
-           }
-         }else{
-           // display error msg
-         }
-       })
-       .catch(err => {
-         console.log(err);
-       });
-     };
-
-
-    // call API to get recipes
-    // const handleSubmit = (event) => {
-    //   console.log("handleSubmit called");
-    //   event.preventDefault();
-    //    fetch(global.config.api_url+"/search_recipes.php?query="+inputText, {
-    //      "method": "GET",
-    //      "headers": {
-    //        "content-type": "text/plain",
-    //        "accept": "application/json"
-    //      }
-    //    })
-    //    .then(response => response.json())
-    //    .then(response => {
-    //      console.log(response);
-    //      // display recipes
-    //      if(response['success'] == true){
-    //        // check results length
-    //        if(response['data'].length > 0){
-    //          // display each result
-    //          for(let i = 0; i < response['data'].length; i++){
-    //            // response['data'][i]['title']
-    //          }
-    //        }else{
-    //          // display no results msg
-    //        }
-    //      }else{
-    //        // display error msg
-    //      }
-    //    })
-    //    .catch(err => {
-    //      console.log(err);
-    //    });
-    //  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit called");
+    fetch(global.config.api_url + "/search_recipes.php?query=" + inputText, {
+      method: "GET",
+      headers: {
+        "content-type": "text/plain",
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        // display recipes
+        if (response["success"] === true) {
+          // check results length
+          if (response["data"].length > 0) {
+            // display each result
+            for (let i = 0; i < response["data"].length; i++) {
+              setRecipes(response["data"][i]);
+            }
+          } else {
+            // display no results msg - TO DO
+          }
+        } else {
+          // display error msg
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="main">
@@ -88,31 +51,34 @@ const FoodSearch = () => {
 
       <div>
         <form autoComplete="off" onSubmit={handleSubmit}>
-
-          {/* Commented out because it makes the page blank, working with the input field below instead - not sure if this is tied to the issue? */}
-          {/* <TextField className="search"
-            id="outlined-basic"
+          <input
+            className="search"
+            type="text"
             onChange={inputHandler}
-            variant="outlined"
-            fullWidth
-            label="Search"/> */}
+            placeholder="Search"
+          />
 
-          <input className="search" type="text" onChange={inputHandler} placeholder="Search"  />
-
-          <button disabled={!inputText} className="foodSearchButton">Click me</button>
+          <button disabled={!inputText} className="foodSearchButton">
+            Click me
+          </button>
         </form>
 
         <div id="recipe-results" className="searchResults">
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <img src={recipe.image}/>
-          </div>
-        ))}
+          {recipes.map((recipe) => (
+            <div key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <img
+                className="searchResultsImage"
+                src={recipe.image}
+                alt={recipe.title}
+                onClick={(event) => (window.location.href = "/some-recipe")}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default FoodSearch;

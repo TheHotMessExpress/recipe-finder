8526806643a7e380ecdfa72566f864c8c12c47c7
jjjjess <91393792+jjjjess@users.noreply.github.com>
2023-03-10 17:17:04 -0500

    Feature - Meet the Developers expandable section implemented
    - Did research on how to implement expandable section containing our "Meet The Developers" cards
    - installed proper library to run "Collapsible"
    - Added filler divs containing lorem ipsum for now, will be updated with proper information once collected
    - css styling partially done

--- frontend/recipeapp/src/pages/Home.js
+++ frontend/recipeapp/src/pages/Home.js
@@ -1,13 +1,63 @@
 import React from "react";
+import Collapsible from "react-collapsible";
   
 const Home = () => {
+
   return (
     <div>
       <h1>
       This is the "Home" Page
       </h1>
+
+      <p>SITE INFO</p>
+
+
+    <div id="collapse">
+      <Collapsible trigger="*** Meet The Developers ***">
+        <div id="float-container">
+        <div id="developerCard">Andrew
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
+          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
+          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
+          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
+          Duis et feugiat nisl. Vestibulum at. </p>
+        </div>
+        <div id="developerCard">Camden
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
+          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
+          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
+          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
+          Duis et feugiat nisl. Vestibulum at. </p>
+        </div>
+        <div id="developerCard">Jesse
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
+          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
+          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
+          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
+          Duis et feugiat nisl. Vestibulum at. </p>
+        </div>
+        <div id="developerCard">Miranda
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
+          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
+          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
+          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
+          Duis et feugiat nisl. Vestibulum at. </p>
+        </div>
+        </div>
+      </Collapsible>
+      </div>
+
+      
+
     </div>
+
+    
   );
 };
+
+
+ 
+
+
   
 export default Home;

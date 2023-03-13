1f609f4b77359e0d21a96a5a7d4773e864a91d13
jjjjess <91393792+jjjjess@users.noreply.github.com>
2023-03-13 16:51:16 -0400

    Feat: details added to meet the developers card
    - Added Jesse and Andrew's pictures to the developer cards, used "goofy" for placeholder for other two cards
    - descriptions added
    - added id's for all tags
    - added lorem to <p> tag to make it more clear for Cam what needs to be changed, also used to test some Collapsible functionality

--- frontend/recipeapp/src/pages/Home.js
+++ frontend/recipeapp/src/pages/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Collapsible from "react-collapsible";
+import goofy from "../images/goofy.webp";
+
+import andrew from "../images/andrew.png";
+import jesse from "../images/jesse.jpg";
   
 const Home = () => {
 
@@ -9,39 +13,33 @@ const Home = () => {
       This is the "Home" Page
       </h1>
 
-      <p>SITE INFO</p>
+      <p id="siteDescription">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+         deserunt mollit anim id est laborum."</p>
 
 
     <div id="collapse">
       <Collapsible trigger="*** Meet The Developers ***">
         <div id="float-container">
-        <div id="developerCard">Andrew
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
-          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
-          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
-          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
-          Duis et feugiat nisl. Vestibulum at. </p>
+        <div id="developerCard"><h1 id="developerName">Andrew</h1>
+        <img id="developerPicture" src={andrew}></img>
+        <p id="developerDiscription">This is my final semester at EMU, majoring in Computer Science. I enjoy designing databases and backend functionality.</p>
         </div>
-        <div id="developerCard">Camden
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
-          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
-          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
-          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
-          Duis et feugiat nisl. Vestibulum at. </p>
+        <div id="developerCard"><h1 id="developerName">Camden</h1>
+        <img  src={goofy}></img>
+        <p id="developerDiscription">Currently in my second to last semester at EMU. Graduating in Fall 2023 with a BS in Computer Science. </p>
         </div>
-        <div id="developerCard">Jesse
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
-          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
-          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
-          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
-          Duis et feugiat nisl. Vestibulum at. </p>
+        <div id="developerCard"><h1 id="developerName">Jesse</h1>
+        <img id="developerPicture" src={jesse}></img>
+        <p id="developerDiscription">I am starting my final year at EMU, graduating with a BA in Computer Science in the Summer or Fall semester.</p>
         </div>
-        <div id="developerCard">Miranda
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras massa tellus, 
-          finibus eu blandit sit amet, interdum sit amet nulla. Donec eget mollis elit, 
-          nec porttitor purus. Curabitur nec diam a lectus ultricies laoreet. 
-          Praesent posuere accumsan eros tempor eleifend. Donec hendrerit pellentesque mollis.
-          Duis et feugiat nisl. Vestibulum at. </p>
+        <div id="developerCard"><h1 id="developerName">Miranda</h1>
+        <img  src={goofy}></img>
+        <p id="developerDiscription">I have 2 more classes at emu. My Major is computer science. I plan to help on the front end with this project </p>
         </div>
         </div>
       </Collapsible>

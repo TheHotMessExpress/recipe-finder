46718063ceb525e1bd2e3bc55217fb7f54cf5f25
Andrew Moots <amoots@emich.edu>
2023-03-29 15:20:32 -0400

    feat: integrate frontend and backend for login
    * alerts user if error
    * redirects to pantry if logged in

--- frontend/recipeapp/src/pages/Login.js
+++ frontend/recipeapp/src/pages/Login.js
@@ -1,26 +1,85 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
   
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // disable button to prevent duplicate submissions
+    document.getElementById("loginButton").style.display = "none";
+    
+    // register with backend
+    console.log(global.config.api_url + "/login.php?email=" + email + "&password=" + password);
+    fetch(global.config.api_url + "/login.php?email=" + email + "&password=" + password, {
+      method: "GET",
+      headers: {
+        "content-type": "text/plain",
+        accept: "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response['success'] === true) {
+        // save token
+        localStorage.setItem("user_token", response['data']['token']);
+
+        // redirect
+        alert("You are now logged in. Your pantry is ready.");
+        window.location.href = "/pantry";
+      } else {
+        // display error
+        alert("Invalid email or password.");
+      }
+
+      // show button again
+      document.getElementById("loginButton").style.display = "";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
   return (
     <div>
       <h1>
       This is the "Login" Page
       </h1>
-
-    <p id="login">
-              Username:
-              <input class="username" type="text"></input>
-              <br></br><br></br>
-            Password:
-              <input class="password" type="text"></input>
-              <br></br><br></br>
-              <button type="button">Log In</button>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-
-              <button type="button">Forgot password (Doesn't work for now)</button>
-              
-    </p>
-
+        <form onSubmit={handleSubmit}>
+          <p id="login">
+                Username:
+                <input className="username" 
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                ></input>
+                <br></br><br></br>
+                Password:
+                <input className="password" 
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  minLength={8}
+                ></input>
+                <br></br><br></br>
+                <button  id="loginButton"
+                  type="submit"
+                >Log In</button>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <button
+                  type="button" 
+                  disabled
+                >Forgot password</button>
+          </p>
+        </form>
+        <Link id="aLink" to="/register">
+          <div>Need to create an account? click here</div>
+        </Link>
     </div>
   );
 };
